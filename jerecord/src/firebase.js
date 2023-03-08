b2e2263d268355c152d50f2053bcb63682be24f9
cytoplasma <jpiromg@gmail.com>
2023-03-08 05:45:41 -0800

    firebase env processes

--- jerecord/src/firebase.js
+++ jerecord/src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHfq6Jj9E3oxDSTYO8mMTaAuu4WUV4ZE8",
-  authDomain: "jchat-app-e4516.firebaseapp.com",
-  projectId: "jchat-app-e4516",
-  storageBucket: "jchat-app-e4516.appspot.com",
-  messagingSenderId: "4801093806",
-  appId: "1:4801093806:web:677d31f601d40c9f428d3f",
-  measurementId: "G-GW9HXYSDSF"
+  apiKey: process.env.FIREBASE_API,
+  authDomain: process.env.FIREBASE_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 
